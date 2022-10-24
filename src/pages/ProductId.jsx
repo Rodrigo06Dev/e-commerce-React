81ce3b3455cf4faf63c2366df7ada6acaad9842f
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2022-10-24 16:27:38 -0500

    Slider

--- src/pages/ProductId.jsx
+++ src/pages/ProductId.jsx
@@ -3,28 +3,32 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ProductInfo from '../components/home/productId/ProductInfo'
 import SimilarProduct from '../components/home/productId/SimilarProduct'
+import SliderImgs from '../components/home/productId/SliderImgs'
 
 const ProductId = () => {
-    const [product, setProduct] = useState()
-    const {id} = useParams()
-    console.log(id);
+  const [product, setProduct] = useState()
+  const { id } = useParams()
+  console.log(id);
 
-    useEffect(() => {
-        const URL = `https://ecommerce-api-react.herokuapp.com/api/v1/products/${id}`
-        axios.get(URL)
-        .then(res => setProduct(res.data.data.product))
-        .catch(err => console.log(err))
-    }, [id])
+  useEffect(() => {
+    const URL = `https://ecommerce-api-react.herokuapp.com/api/v1/products/${id}`
+    axios.get(URL)
+      .then(res => setProduct(res.data.data.product))
+      .catch(err => console.log(err))
+  }, [id])
 
-    console.log(product);
+  console.log(product);
   return (
     <div>
-        <ProductInfo
-            product = {product}
-        />
-        <SimilarProduct
-           product = {product}
-        />
+      {
+        product && <SliderImgs product={product} />
+      }
+      <ProductInfo
+        product={product}
+      />
+      <SimilarProduct
+        product={product}
+      />
     </div>
   )
 }
