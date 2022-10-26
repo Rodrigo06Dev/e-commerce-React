d3a60eafcd26688162a96167eda20e4500dcb81d
MansuttiD <daniel.mansutti@gmail.com>
2022-10-25 21:19:38 -0400

    vista del producto, logo y footer

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './styles/footer.css'
+
+const Footer = () => {
+
+  return (
+    <footer className='footer__container'>
+        <div className='footer__icon'>
+        <i class="fa-brands fa-instagram"></i>
+        </div>
+        <div className='footer__icon'>
+        <i class="fa-brands fa-linkedin"></i>
+        </div>
+        <div className='footer__icon'>
+        <i class="fa-brands fa-twitter"></i>
+        </div>
+
+    </footer>
+
+  )
+}
+
+export default Footer
